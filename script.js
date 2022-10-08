b8333b02c37db3a9b6fbe41f93e71d070a73b458
Axel Stenson <axel@Axels-Air.zyxel.com>
2022-10-07 20:58:05 -0500

    update a few values

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-colorlist = ['red', 'blue'];
+colorlist = ['deepskyblue', 'blue'];
 
 const cols = 60;
 const rows = 60;
@@ -15,87 +15,90 @@ function bool2Int(flag) {
 function printNumbers() {
   x = (width) / cols
   y = (height) / rows
-  
+
   for (let i = 0; i < cols; i += 1) {
 
     for (let j = 0; j < rows; j += 1) {
       push();
       translate(i * x, j * y);
 
-      if (running){
+      if (running) {
         nestedArray[j][i] = runRules(nestedArray[j][i], j, i);
       }
-      
-      //if weird, switch j and i around
-      if (nestedArray[j][i]){
-        stroke(colorlist[1]);
-      }
-      text(bool2Int(nestedArray[j][i]).toString(), 0, 0);
 
-      stroke(colorlist[1]);
+      //if weird, switch j and i around
+      if (nestedArray[j][i]) {
+        stroke(colorlist[1]);
+        text("°", 0, 0)
+      }
+      else {
+        stroke(colorlist[0]);
+        text("°", 0, 0);
+      }
+
       pop();
     }
   }
-}  
+}
 
 function changeOnMouseOver() {
-  if (mouseX >= 0 && mouseY >= 0){
-      nestedArray[(Math.round((mouseY/height) * rows) % rows)][(Math.round((mouseX/width) * cols) % cols)] = true;
+  if (mouseX >= 0 && mouseY >= 0) {
+    nestedArray[(Math.round((mouseY / height) * rows) % rows)][(Math.round((mouseX / width) * cols) % cols)] = true;
   }
 }
 
-function runRules(cell, xAddress, yAddress){
+function runRules(cell, xAddress, yAddress) {
   neighborCount = 0;
   // Check the left most column
-  if (nestedArray[((xAddress - 1 + cols) % cols)][(yAddress + 1) % rows]){
+  if (nestedArray[((xAddress - 1 + cols) % cols)][(yAddress + 1) % rows]) {
     neighborCount++;
   }
-  if (nestedArray[((xAddress - 1 + cols) % cols)][yAddress]){
+  if (nestedArray[((xAddress - 1 + cols) % cols)][yAddress]) {
     neighborCount++;
   }
-  if (nestedArray[((xAddress - 1 + cols) % cols)][(yAddress - 1 + rows) % rows]){
+  if (nestedArray[((xAddress - 1 + cols) % cols)][(yAddress - 1 + rows) % rows]) {
     neighborCount++;
   }
 
   // Check the middle column
-  if (nestedArray[xAddress][(yAddress + 1) % rows ]){
+  if (nestedArray[xAddress][(yAddress + 1) % rows]) {
     neighborCount++;
   }
-    if (nestedArray[xAddress][(yAddress - 1 + rows) % rows]){
+  if (nestedArray[xAddress][(yAddress - 1 + rows) % rows]) {
     neighborCount++;
   }
 
   // Check the right most column
-  if (nestedArray[(xAddress + 1) % cols][(yAddress + 1) % rows]){
+  if (nestedArray[(xAddress + 1) % cols][(yAddress + 1) % rows]) {
     neighborCount++;
   }
-    if (nestedArray[(xAddress + 1) % cols][yAddress]){
+  if (nestedArray[(xAddress + 1) % cols][yAddress]) {
     neighborCount++;
   }
-    if (nestedArray[(xAddress + 1) % cols][(yAddress - 1 + rows) % rows]){
+  if (nestedArray[(xAddress + 1) % cols][(yAddress - 1 + rows) % rows]) {
     neighborCount++;
   }
 
   if (cell &&
-    ((neighborCount < 2 ) ||
-    (neighborCount > 3))){
-      return false;
+    ((neighborCount < 2) ||
+      (neighborCount > 3))) {
+    return false;
   }
-  if (!cell && 
-    (neighborCount == 3)){
-      return true;
+  if (!cell &&
+    (neighborCount == 3)) {
+    return true;
   }
   else
     return cell;
 }
 
-function flipSwitch(){
+function flipSwitch() {
   running = !running;
 }
 
 
 function setup() {
-  
+
   createCanvas(windowWidth, windowHeight);
   background('white');
   fill('white');
@@ -108,20 +111,20 @@ function setup() {
   colorIndex = 0;
   running = false;
 
-  
+
   var btn = document.getElementById("switch");
   btn.addEventListener('click', () => {
-  flipSwitch();
+    flipSwitch();
   })
 }
 
 
 function draw() {
 
-  if (frameCount%3 == 0){
+  if (frameCount % 3 == 0) {
     printNumbers();
   }
-  
+
   changeOnMouseOver();
-  
+
 }
